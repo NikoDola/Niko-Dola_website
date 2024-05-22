0d74156cee369028dfe45a7b7d7e0bfbd8a402d1
Nikola Dolovski <nikodola@gmail.com>
2024-05-22 16:29:45 +0200

    day 2

---
+++ test/test.js
@@ -0,0 +1,44 @@
+const lista = [1, 4, 6, ,2 ,3 ,5]
+
+lista.pop('nikola')
+console.log(lista)
+// const women = ['Natasha', 'Vesna', 'Radica']
+// const womenLenght = women.length
+
+// let newa = ''
+
+
+// women.map
+// console.log(newa)
+// // const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// document.getElementById("text").innerHTML = fruits;
+
+
+// const cars = []
+// cars[0]= "nikola"
+// cars[1] = "igor"
+// cars[2] = "peckata"
+// console.log(cars)
+
+
+// const cars = new Array('nikola', 'peckata', 'dejan', 'marijan')
+// console.log(cars)
+
+// const cars = ["Saab", "Volvo", "BMW"];
+// let car = cars[0];
+// console.log(car)
+
+
+
+// Changing an Array Element
+
+
+
+// const cars = ["saab", "volvo", "BWM"];
+// cars[0] = "opel"
+
+// cars.push = 'ficho'
+
+// cars = 'aman'
+// console.log(cars)
+
