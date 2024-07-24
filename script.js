9b468b887efe78f3f5e483aff5ae9101930f1c65
nikola <nikodola@gmail.com>
2024-07-24 21:35:08 +0200

    oreder logo

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const buttonCode2 = document.getElementById('buttonCode2');
 const buttonCode3 = document.getElementById('buttonCode3');
 
 const arrowCode1 = document.getElementById('arrowCode1');
-const arrowCode2= document.getElementById('arrowCode2');
+const arrowCode2 = document.getElementById('arrowCode2');
 const arrowCode3 = document.getElementById('arrowCode3');
 
 const contentCode1 = document.getElementById('contentCode1');
@@ -13,7 +13,6 @@ const contentCode3 = document.getElementById('contentCode3');
 const secretButtonCode1 = document.getElementById('secretButtonCode1');
 const secretButtonCode2 = document.getElementById('secretButtonCode2');
 const secretButtonCode3 = document.getElementById('secretButtonCode3');
-
 
 const submit = document.getElementById('submit');
 
@@ -48,11 +47,8 @@ const colorData = colorInfo.textContent = colorSet;
 //     }
 // });
 
-
 // Button 2
 let toggleCode2 = false;
-
-
 
 secretButtonCode2.addEventListener('click', () => {
     if (!toggleCode2) {
@@ -74,8 +70,6 @@ secretButtonCode2.addEventListener('click', () => {
         contentCode2.style.opacity = '0';
     }
 });
-
-
 
 let toggleCode3 = false;
 
@@ -100,57 +94,57 @@ secretButtonCode3.addEventListener('click', () => {
     }
 });
 
+function logoChangeColors() {
+    const logo = document.getElementById('logo');
+    const instagram = document.getElementById('instagram');
+    const youtube = document.getElementById('youtube');
+    const pinterest = document.getElementById('pinterest');
+    const linkedin = document.getElementById('linkedin');
+    const dribbble = document.getElementById('dribbble');
+    const beehance = document.getElementById('beehance');
 
+    instagram.addEventListener('mouseover', () => {
+        logo.style.background = '#f56319';
+    });
+    instagram.addEventListener('mouseout', () => {
+        logo.style.background = '#87CFD3';
+    });
 
-function logoChangeColors(){
-const logo = document.getElementById('logo');
-const instagram = document.getElementById('instagram');
+    youtube.addEventListener('mouseover', () => {
+        logo.style.background = 'red';
+    });
+    youtube.addEventListener('mouseout', () => {
+        logo.style.background = '#87CFD3';
+    });
 
+    pinterest.addEventListener('mouseover', () => {
+        logo.style.background = 'red';
+    });
+    pinterest.addEventListener('mouseout', () => {
+        logo.style.background = '#87CFD3';
+    });
 
-instagram.addEventListener('mouseover', ()=>{
-    logo.style.background='#f56319';
-})
-instagram.addEventListener('mouseout', ()=>{
-    logo.style.background = '#87CFD3';
-})
+    linkedin.addEventListener('mouseover', () => {
+        logo.style.background = '#0A66C2';
+    });
+    linkedin.addEventListener('mouseout', () => {
+        logo.style.background = '#87CFD3';
+    });
 
-youtube.addEventListener('mouseover', ()=>{
-    logo.style.background='red';
-})
-youtube.addEventListener('mouseout', ()=>{
-    logo.style.background = '#87CFD3';
-})
+    dribbble.addEventListener('mouseover', () => {
+        logo.style.background = '#EA4C89';
+    });
+    dribbble.addEventListener('mouseout', () => {
+        logo.style.background = '#87CFD3';
+    });
 
-pinterest.addEventListener('mouseover', ()=>{
-    logo.style.background='red';
-})
-pinterest.addEventListener('mouseout', ()=>{
-    logo.style.background = '#87CFD3';
-})
-
-linkedin.addEventListener('mouseover', ()=>{
-    logo.style.background='#0A66C2';
-})
-linkedin.addEventListener('mouseout', ()=>{
-    logo.style.background = '#87CFD3';
-})
-
-dribbble.addEventListener('mouseover', ()=>{
-    logo.style.background='#EA4C89';
-})
-dribbble.addEventListener('mouseout', ()=>{
-    logo.style.background = '#87CFD3';
-})
-
-beehance.addEventListener('mouseout', ()=>{
-    logo.style.background = '#87CFD3';
-})
-
-beehance.addEventListener('mouseover', ()=>{
-    logo.style.background='#0A66C2';
-})}
-
-
+    beehance.addEventListener('mouseover', () => {
+        logo.style.background = '#0A66C2';
+    });
+    beehance.addEventListener('mouseout', () => {
+        logo.style.background = '#87CFD3';
+    });
+}
 
 const contactName = document.getElementById('contactName');
 const contactEmail = document.getElementById('contactEmail');
@@ -162,72 +156,60 @@ const hasNotInput = document.getElementById('hasNot');
 const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
 
-const submitPopUp = document.getElementById('submitPopUp')
-const submitPopUp2 = document.getElementById('submitPopUp2')
-
-
-
+const submitPopUp = document.getElementById('submitPopUp');
+const submitPopUp2 = document.getElementById('submitPopUp2');
 
 console.log(contactEmail.value);
 
-
-function ContactEmail(){
-grecaptcha.getResponse()
+function ContactEmail() {
+    grecaptcha.getResponse();
     Email.send({
-        
-        Host : "smtp.elasticemail.com",
-        Username : "nikodola@gmail.com",
-        Password : "A15A80FEFB7C3FF1665E521EDB3C1500EFA6",
-        To : 'dolovska@hotmail.com',
-        From : "nikodola@gmail.com",
-        Subject : "Contact US ",
-        Body : `Message: ${contactMessage.value}, 
+        Host: "smtp.elasticemail.com",
+        Username: "nikodola@gmail.com",
+        Password: "A15A80FEFB7C3FF1665E521EDB3C1500EFA6",
+        To: 'dolovska@hotmail.com',
+        From: "nikodola@gmail.com",
+        Subject: "Contact US",
+        Body: `Message: ${contactMessage.value}, 
         Email: ${contactEmail.value}, Name: ${contactName.value}`
     }).then(
         submitPopUp2.style.display = 'block'
     );
-};
+}
 
-    function LogoEmail(e){
-        
-        e.preventDefault()
-        // const recaptchaResponse = grecaptcha.getResponse();
+function LogoEmail(e) {
+    e.preventDefault();
+    // const recaptchaResponse = grecaptcha.getResponse();
 
-        // if (recaptchaResponse.length === 0) {
-        // alert('Please complete the reCAPTCHA.');
-        // } else {
-        // // Proceed with form submission
-        // this.submit();
-        // }
-        const colorAray = Array.from(colorSet).join(', '); 
-        const inspirationAray = Array.from(inspirationSet).join(', '); 
-    
-            const params = {
-                companyName: companyNameInput.value,
-                tagLine: taglineInput.value,
-                has: hasInput.value,
-                hasNot: hasNotInput.value,
-                color: colorAray,
-                inspiration: inspirationAray,
-                name: nameInput.value,
-                email: emailInput.value
-            };
+    // if (recaptchaResponse.length === 0) {
+    // alert('Please complete the reCAPTCHA.');
+    // } else {
+    // // Proceed with form submission
+    // this.submit();
+    // }
+    const colorAray = Array.from(colorSet).join(', ');
+    const inspirationAray = Array.from(inspirationSet).join(', ');
 
-            // Sending email
-            emailjs.send("service_3r25scb", "template_7m9enxl", params)
-                .then(function(response) {
-                    console.log('SUCCESS!', response.status, response.text);
-                    submitPopUp.style.display = 'block';
-                }, function(error) {
-                    console.log('FAILED...', error);
-                    submitPopUp.textContent = `Fail to send message because of ${error}`
-                    submitPopUp.style.color = 'red'
-                    submitPopUp.style.display = 'block';
-                    
-                });
-    }
+    const params = {
+        companyName: companyNameInput.value,
+        tagLine: taglineInput.value,
+        has: hasInput.value,
+        hasNot: hasNotInput.value,
+        color: colorAray,
+        inspiration: inspirationAray,
+        name: nameInput.value,
+        email: emailInput.value
+    };
 
-
-
-
-
+    // Sending email
+    emailjs.send("service_3r25scb", "template_7m9enxl", params)
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+            submitPopUp.style.display = 'block';
+        }, function (error) {
+            console.log('FAILED...', error);
+            submitPopUp.textContent = `Fail to send message because of ${error}`;
+            submitPopUp.style.color = 'red';
+            submitPopUp.style.display = 'block';
+        });
+}
